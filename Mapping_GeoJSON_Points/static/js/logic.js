11620fba31540cf36abec778ea7404f004f146f3
Diamond Washington <Washington3.diamond@gmail.com>
2021-12-20 20:42:48 -0500

    import JSON data

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -3,8 +3,9 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with center at the San Francisco airport.
-let map = L.map('mapid').setView([37.5, -122.5], 10);
+// Create the map object with center and zoom level.
+let map = L.map('mapid').setView([30, 30], 2);
+
 
 // Add GeoJSON data.
 let sanFranAirport =
@@ -26,19 +27,21 @@ let sanFranAirport =
             "coordinates":[-122.375,37.61899948120117]}}
 ]};
 
-// Grabbing our GeoJSON data.
-L.geoJSON( sanFranAirport, {
-    onEachFeature: function(feature, layer) {
-        console.log(layer);
-        layer.bindPopup("<h2>" +"Airport code: "+ feature.properties.faa+ "<br>"+ "Airport name: "+feature.properties.name + "</br>" + "</h2>");
-    }
-}).addTo(map);
-
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: api_key
+});
+
+// Accessing the airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/DWashington3/Mapping_Earthquakes/main/majorAirports.json";
+
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data) {
+    console.log(data);
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data).addTo(map);
 });
 
 // Then we add our 'graymap' tile layer to the map.
